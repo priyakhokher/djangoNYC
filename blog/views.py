ea89b1afbafe3cc0f3fcce76a6e73eb3556495b1
priyakhokher <pnk230@nyu.edu>
2016-02-27 17:36:46 -0800

    finalTrial

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm 
 from django.shortcuts import redirect
+
 # Create your views here.
 
 def post_detail(request, pk):
