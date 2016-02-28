69e745609be4bdeb3be56bfa4324b581c152be8f
priyakhokher <pnk230@nyu.edu>
2016-02-27 17:47:00 -0800

    changesWSGI.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from .forms import PostForm 
 from django.shortcuts import redirect
 
